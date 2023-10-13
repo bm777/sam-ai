b4f61a169234365cefe4d6af658d349a08f36b80
Bayangmbe Mounmo <bayangp0@gmail.com>
2023-10-13 04:52:11 +0200

    added sample of generate  endpoint

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
+import requests
+from sam_ai import get_answer
 
 @app.route('/')
 def hello_world():
     return 'basic API'
+
+
+@app.route('/generate', methods=['GET'])
+def generate():
+    """
+    Handles the /generate endpoint which generate answer based on a given query sementically.
+    """
+    query = request.args.get('query')
+    
+    row = get_answer(query)
+    
+    if row:
+        response = {
+            'answer': row[0] if row else None,
+        'sources': row[1] if row else None,
+        'image_url': row[2] if row else None,
+        'error': None if row else 'No answer could not be generated from the user query'
+        }
+    else:
+        response = {'error': 'No answer could not be generated from the user query'}
+    
+    return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ sam_ai/sam.py
@@ -0,0 +1,11 @@
+def get_answer(query):
+    """
+    Generate answer, sources, and image_url from the database based on the given query.
+    For the Frontend sam-ui, the query will be the user's input.
+    Args:
+        query (str): The query for fetching data from the database.
+    Returns:
+        tuple: A tuple containing the answer, sources, and image_url, or None if not found.
+    """
+
+    return ['fake answer', 'fake sources', 'fake image_url']
